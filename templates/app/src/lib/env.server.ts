f966fc8cc2abe0c6238a562400447b5d2e6207a6
Roy Portas <royportas@gmail.com>
2026-07-30 08:27:37 +1000

    Remove the test variable

--- templates/app/src/lib/env.server.ts
+++ templates/app/src/lib/env.server.ts
@@ -3,8 +3,6 @@ import { z } from "zod";
 /**
  * Server-only environment variables.
  */
-const serverEnvSchema = z.object({
-  TEST: z.string().min(1, "TEST is required"),
-});
+const serverEnvSchema = z.object({});
 
 export default serverEnvSchema.parse(process.env);
